9159ba02b08844acd8af5c3dc2121e609f0dfff1
Aman Mohammad <118279443+amann-273@users.noreply.github.com>
2022-11-27 15:02:54 +0530

    Add files via upload

---
+++ largest.py
@@ -0,0 +1,20 @@
+n1=input()
+n1=int()
+
+n2=input()
+n2=int()
+
+n3=input()
+n3=int()
+
+if(n1>=n2 and n1>=n3):
+    print("n1 is largest")
+
+elif (n2>=n1 and n2>=n3):
+    print("n2 is largest")
+
+elif(n3>=n1 and n3>n2):
+ print("n3 is largest")
+else:
+ print("yayy")
+
